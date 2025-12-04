ada8a829fe85f2ec14522c396a200642ed84ae65
Weston Macke <westonmacke@Westons-Mini.localdomain>
2025-12-03 21:00:17 -0600

    Started adding Nav components and authentication

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-layout>
+      <MainNav />
+
+      <v-main style="height: 100vh">
+        <v-container fluid>
+          <h1>Main Content Area</h1>
+          <p>This is where your main content will go.</p>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script setup>
+import MainNav from './MainNav.vue'
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-navigation-drawer
+    expand-on-hover
+    rail
+    location="right"
+  >
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+        title="John Leider"
+      >
+        <template v-slot:append>
+          <v-btn
+            icon="mdi-cog"
+            variant="text"
+            @click="handleSettings"
+          ></v-btn>
+        </template>
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-file-document-multiple" title="Intraforms" value="intraforms"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+// Main Navigation Drawer Component
+
+const handleSettings = () => {
+  // TODO: Setup settings function
+  console.log('Settings clicked')
+}
+</script>
+
+<style scoped>
+</style>
